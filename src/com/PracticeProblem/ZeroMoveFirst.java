b2f4f8e8fec53c576a683236c6a00a1c890b0829
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-26 23:45:15 +0530

    first_program7.

---
+++ src/com/PracticeProblem/ZeroMoveFirst.java
@@ -0,0 +1,20 @@
+package com.PracticeProblem;
+
+import java.util.Arrays;
+
+public class ZeroMoveFirst {
+    public static void main(String[] args){
+        int arr[] = {2,0,4,0,5,0,6,0};
+        int arr1[] = new int[arr.length];
+        int count = arr1.length-1;
+        for(int i = arr.length-1; i>=0; i--){
+            if(arr[i] != 0){
+                arr1[count--] = arr[i];
+            }
+        }
+
+            System.out.println(Arrays.toString(arr1));
+
+
+    }
+}
