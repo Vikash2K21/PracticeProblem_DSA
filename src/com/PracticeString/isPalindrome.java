644938d983984f8aecc9185867f183fa9f085d70
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-27 20:21:59 +0530

    first_program3.

---
+++ src/com/PracticeString/isPalindrome.java
@@ -0,0 +1,24 @@
+package com.PracticeString;
+
+public class isPalindrome {
+    public static void main(String[] args) {
+        String s = "mamojmam";
+
+        int left = 0;
+        int right = s.length()-1;
+        boolean flag = true;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                flag = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if(flag){
+            System.out.println("palinrome");
+        }else{
+            System.out.println("Not palindrome");
+        }
+    }
+}
