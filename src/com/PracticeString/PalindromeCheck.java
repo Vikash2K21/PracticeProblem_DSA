e908682d82efc90d1f5d307a7467ba8981a763da
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-27 19:15:08 +0530

    first_program3.

---
+++ src/com/PracticeString/PalindromeCheck.java
@@ -0,0 +1,17 @@
+package com.PracticeString;
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String s = "maaamam";
+        String original = s;
+        String rev = "";
+        for(int i=s.length()-1; i>=0; i--){
+            rev +=s.charAt(i);
+        }
+        if (original.equals(rev)) {
+
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("not palindrome");
+        }
+    }
+}
