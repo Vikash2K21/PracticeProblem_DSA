6b2b886955a4eaac3ebe8f0b637edfc12fad4149
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-28 16:57:08 +0530

    first_program3.

---
+++ src/com/PracticeProblem/fibonacchiSeries.java
@@ -0,0 +1,18 @@
+package com.PracticeProblem;
+
+public class fibonacchiSeries {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=10;
+        int c = 0;
+        System.out.print(a+" "+b+" ");
+        for(int i=2; i<=n; i++){
+            c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+
+    }
+}
