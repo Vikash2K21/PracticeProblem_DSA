784af7dd1aa1a2b34af66779a45e888fc42574d2
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-28 14:46:17 +0530

    first_program3.

---
+++ src/com/PracticeString/UpToLow.java
@@ -0,0 +1,18 @@
+package com.PracticeString;
+
+public class UpToLow {
+    public static void main(String[] args) {
+        String s = "ViKaSh";
+        String str = "";
+        char[] ch = s.toCharArray();
+        for(int i=0;i<s.length(); i++){
+            if(ch[i] >='A' && ch[i] <='Z'){
+                ch[i] += 32;
+            }else if(ch[i] >='a' && ch[i]<='z'){
+                ch[i] -= 32;
+            }
+        }
+        System.out.println(s);
+        System.out.println(ch);
+    }
+}
