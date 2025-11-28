80db6c5f51f9016435cb939510bf9e0e684d5c5b
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-28 17:23:28 +0530

    first_program4.

---
+++ src/com/PracticeString/ReplaceMethod.java
@@ -0,0 +1,9 @@
+package com.PracticeString;
+
+public class ReplaceMethod {
+    public static void main(String[] args) {
+        String s = "Vikash";
+        s = s.replace("a","u");
+        System.out.println(s);
+    }
+}
