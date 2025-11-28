00034ec17070864eac775e8c1149f03d5094022b
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-28 15:32:38 +0530

    first_program3.

---
+++ src/com/PracticeString/RemoveDuplicateCharacter.java
@@ -0,0 +1,19 @@
+package com.PracticeString;
+
+public class RemoveDuplicateCharacter {
+    public static void main(String[] args) {
+        String s = "javadev";
+        String res = "";
+        char []ch = s.toCharArray();
+        for(int i=0; i<s.length(); i++){
+            if(ch[i] == '\u0000')continue;
+            for(int j =i+1;j<s.length(); j++){
+                if(ch[i] == ch[j]){
+                    ch[j] = '\u0000';
+                }
+            }
+            res +=ch[i];
+        }
+        System.out.println(res);
+    }
+}
